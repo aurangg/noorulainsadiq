1fd6e52b62e65f2e3a99d6f1528f0298d22e0732
Aurangzeb Khan <aurangzebbbk@gmail.com>
2024-06-15 05:01:11 +0500

    letter push

---
+++ app/drafts/page1.tsx
@@ -0,0 +1,77 @@
+"use client";
+import "bootstrap/dist/css/bootstrap.css";
+import { useEffect, useRef, useState } from "react";
+import lottieConfetti from "../../public/confetti.json";
+import Lottie from "lottie-react";
+
+export default function Home() {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isFlipped2, setIsFlipped2] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(true);
+  };
+
+  const handleFlip2 = () => {
+    setIsFlipped2(true);
+  };
+
+  return (
+    <main>
+      <section className="home-bg position-relative">
+        {isFlipped === true || isFlipped2 === true ? (
+          <div className="lottie-cover">
+            <Lottie animationData={lottieConfetti} loop={false} />
+          </div>
+        ) : (
+          <></>
+        )}
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h2 className="main-heading">Hello Noor!</h2>
+              <p className="main-text">I wanted to do something for you.</p>
+              <p className="main-text">Because I miss you so.</p>
+            </div>
+            <div className="col-lg-12">
+              <h2 className="other-heading">Select between two..</h2>
+              <p className="main-text">
+                Upon clicking, you will get to see whats behind the box
+              </p>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-lg-6 offset-lg-3">
+              <div className="space-between">
+                <div
+                  className="perspective"
+                  style={{ display: isFlipped2 === true ? "none" : "" }}
+                >
+                  <div
+                    className={`choice-button ${isFlipped ? "flipped" : ""}`}
+                    onClick={handleFlip}
+                  >
+                    <div className="front">Choice 1</div>
+                    <div className="back">Spotify Jam</div>
+                  </div>
+                </div>
+                <div
+                  className="perspective"
+                  style={{ display: isFlipped === true ? "none" : "" }}
+                >
+                  <div
+                    className={`choice-button ${isFlipped2 ? "flipped" : ""}`}
+                    onClick={handleFlip2}
+                  >
+                    <div className="front">Choice 2</div>
+                    <div className="back">Call Me?</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,73 +1,63 @@
 "use client";
 import "bootstrap/dist/css/bootstrap.css";
-import { useEffect, useRef, useState } from "react";
-import lottieConfetti from "../public/confetti.json";
-import Lottie from "lottie-react";
+import { useState } from "react";
 
 export default function Home() {
-  const [isFlipped, setIsFlipped] = useState(false);
-  const [isFlipped2, setIsFlipped2] = useState(false);
-
-  const handleFlip = () => {
-    setIsFlipped(true);
-  };
-
-  const handleFlip2 = () => {
-    setIsFlipped2(true);
-  };
-
+  const [pinValue, setPinValue] = useState("");
+  const letterData = [
+    {
+      id: 0,
+      pin: "4565",
+      letterText: `Dear Chanda, I hope and pray that you are okay, I really hope and pray so. Today you are back home, to your home, and although it's not the first time you're back at home, it just feels different, very different this time. Not saying that whenever you have been back home, you really needed to be back, but this time, its different. Different perhaps in a sense as if someone has taken away the love of my life from me. I feel sick and it feels like autumn all over the place. I just hope you're okay. This letter series is going to be about each and every single day and today was the first day without you. It feels awful. It is very disturbing and unsettling. I just hope you're okay. I hope I can be the reason of your strength for the surgery that you have to go through. Don't be afraid. I am here with you. I am holding you. As much as I want to be there for you and I hate every single moment where I can't be with you, specially in these difficult times, I promise you this won't last for long. I can't let you face things on your own. You will never face anything, let alone facing something again. I am here, here for you, standing as a wall in front of everything and anything. Everything will have go through me first, while I give you the space to cherish and just live freely, the way you truly deserve. Each call, each message, each voice note and each reaction that came from you, was everything I needed. They were a warm hug, a very warm hug and that is why I was just being grateful. Thank you for everything. Chanda, I am grateful for everything. Grateful to have you as my partner. With each passing day, the urge to be with you is just growing. Only a matter of days till we make it official. I love you.`,
+    },
+  ];
+  function isPinIncluded(pinValue: string) {
+    return letterData.some((letter) => letter.pin === pinValue);
+  }
   return (
     <main>
-      <section className="home-bg position-relative">
-        {isFlipped === true || isFlipped2 === true ? (
-          <div className="lottie-cover">
-            <Lottie animationData={lottieConfetti} loop={false} />
-          </div>
-        ) : (
-          <></>
-        )}
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12">
-              <h2 className="main-heading">Hello Noor!</h2>
-              <p className="main-text">I wanted to do something for you.</p>
-              <p className="main-text">Because I miss you so.</p>
-            </div>
-            <div className="col-lg-12">
-              <h2 className="other-heading">Select between two..</h2>
-              <p className="main-text">
-                Upon clicking, you will get to see whats behind the box
-              </p>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-lg-6 offset-lg-3">
-              <div className="space-between">
-                <div
-                  className="perspective"
-                  style={{ display: isFlipped2 === true ? "none" : "" }}
-                >
-                  <div
-                    className={`choice-button ${isFlipped ? "flipped" : ""}`}
-                    onClick={handleFlip}
-                  >
-                    <div className="front">Choice 1</div>
-                    <div className="back">Spotify Jam</div>
-                  </div>
-                </div>
-                <div
-                  className="perspective"
-                  style={{ display: isFlipped === true ? "none" : "" }}
-                >
-                  <div
-                    className={`choice-button ${isFlipped2 ? "flipped" : ""}`}
-                    onClick={handleFlip2}
-                  >
-                    <div className="front">Choice 2</div>
-                    <div className="back">Call Me?</div>
+      <section>
+        {/* <div
+          className={`overlay ${isPinIncluded(pinValue) ? "no-overlay" : ""}`}
+        >
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-6 offset-lg-3">
+                <div className="overlay-container">
+                  <div className="d-flex" style={{ flexDirection: "column" }}>
+                    <label>Enter Pin</label>
+                    <input
+                      className="input-field"
+                      type="password"
+                      placeholder="Pin"
+                      onChange={(e) => setPinValue(e.target.value)}
+                      required
+                    />
                   </div>
                 </div>
               </div>
+            </div>
+          </div>
+        </div> */}
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-6 offset-lg-3">
+              {letterData.map((i, index) => (
+                <>
+                  <div className="letter-box" key={index}>
+                    <>
+                      <h2 className="letter-heading">
+                        Letter {i.id + 1}/2 weeks
+                      </h2>
+                      <p style={{ marginBottom: "0px" }}>{i.letterText}</p>
+                    </>
+                  </div>
+                  {/* {pinValue === i?.pin ? (
+                  ) : (
+                    <></>
+                  )} */}
+                </>
+              ))}
             </div>
           </div>
         </div>
